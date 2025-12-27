01a69e50ace6fba6be385de687a9e859e01176a3
itscrowley <45391180+itscrowley@users.noreply.github.com>
2025-12-27 13:37:13 +0530

    User.ts

--- models/User.ts
+++ models/User.ts
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <div>Coming soon</div>;
-}
+import mongoose, { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: false },
+    role: { type: String, default: "user" }, // 'admin' or 'user'
+  },
+  { timestamps: true }
+);
+
+export const User = models.User || model("User", UserSchema);
