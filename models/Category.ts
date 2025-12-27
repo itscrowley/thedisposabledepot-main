3c6a1113d9872779bf3aa1007c6abded76a75be1
itscrowley <45391180+itscrowley@users.noreply.github.com>
2025-12-27 13:35:16 +0530

    Category.ts

--- models/Category.ts
+++ models/Category.ts
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <div>Coming soon</div>;
-}
+import mongoose, { Schema, model, models } from "mongoose";
+
+const CategorySchema = new Schema(
+  {
+    name: { type: String, required: true },
+    slug: { type: String, unique: true },
+    description: { type: String },
+  },
+  { timestamps: true }
+);
+
+export const Category = models.Category || model("Category", CategorySchema);
